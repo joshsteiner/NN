0d51f680c363e7d363c15f267809d337376724e0
Joshua Steiner <josh.steiner@protonmail.com>
2018-01-06 12:48:45 +0100

    simplify file reading

--- NN/examples/MNISTreader.cpp
+++ NN/examples/MNISTreader.cpp
@@ -28,11 +28,25 @@ static uint32_t to_uint32_t(char *cs)
 
 std::vector<matrix> MNIST::read_label_file(const char *filename)
 {
-	label_file fr(filename);
+	std::fstream fr(filename, std::ios::in | std::ios::binary);
 	std::vector<matrix> result;
 
-	while (!fr.end())
-		result.push_back(fr.next());
+	char header_buff[2][4];
+
+	fr.read(header_buff[0], 4);
+	fr.read(header_buff[1], 4);
+
+	if (to_uint32_t(header_buff[0]) != 2049)  // magic number
+		return result;
+
+	uint32_t count = to_uint32_t(header_buff[1]);
+	
+	char label_buff[1];
+	for (int i = 0; i < count; ++i)
+	{
+		fr.read(label_buff, 1);
+		result.push_back(one_hot_true(label_buff[0]));
+	}
 
 	fr.close();
 	return result;
@@ -41,100 +55,34 @@ std::vector<matrix> MNIST::read_label_file(const char *filename)
 
 std::vector<matrix> MNIST::read_image_file(const char *filename)
 {
-	image_file fr(filename);
+	std::fstream fr(filename, std::ios::in | std::ios::binary);
 	std::vector<matrix> result;
-
-	while (!fr.end())
-		result.push_back(fr.next());
-
-	fr.close();
-	return result;
-}
-
-
-bool MNIST::file::end() const
-{
-	return fstrm.bad()
-		|| fstrm.eof()
-		|| it == count
-		|| is_end;
-}
-
-
-void MNIST::file::close()
-{
-	fstrm.close();
-}
-
-
-MNIST::label_file::label_file(const char *filename)
-{
-	this->it = 0;
-
-	fstrm.open(filename, std::ios::in | std::ios::binary);
 
 	char header_buff[2][4];
 
-	fstrm.read(header_buff[0], 4);
-	fstrm.read(header_buff[1], 4);
-
-	if (to_uint32_t(header_buff[0]) != magic_number)
-		is_end = true;
-
-	count = to_uint32_t(header_buff[1]);
-}
-
-
-MNIST::label_file::~label_file()
-{
-	close();
-}
-
-
-matrix MNIST::label_file::next()
-{
-	char label_buff[1];
-	fstrm.read(label_buff, 1);
-	++it;
-	return one_hot_true(label_buff[0]);
-}
-
-
-MNIST::image_file::image_file(const char *filename)
-{
-	this->it = 0;
-
-	fstrm.open(filename, std::ios::in | std::ios::binary);
-
-	char header_buff[4][4];
-
 	for (int i = 0; i < 4; ++i)
-		fstrm.read(header_buff[i], 4);
+		fr.read(header_buff[i], 4);
 
-	if (to_uint32_t(header_buff[0]) != magic_number)
-		is_end = true;
+	if (to_uint32_t(header_buff[0]) != 2051)  // magic number
+		return result;
 
-	count = to_uint32_t(header_buff[1]);
-	row_count = to_uint32_t(header_buff[2]);
-	column_count = to_uint32_t(header_buff[3]);
-}
+	uint32_t
+		count = to_uint32_t(header_buff[1]),
+		row_count = to_uint32_t(header_buff[2]),
+		column_count = to_uint32_t(header_buff[3]);
 
-
-MNIST::image_file::~image_file()
-{
-	close();
-}
-
-
-matrix MNIST::image_file::next()
-{
-	matrix image(row_count * column_count, 1);
-	char pixel_buff[1];
-	for (int j = 0; !fstrm.eof() && j < row_count * column_count; ++j)
+	for (int i = 0; i < count; ++i)
 	{
-		fstrm.read(pixel_buff, 1);
-		image.at(j,0) = (double)(uint8_t)pixel_buff[0] / 255.0;
+		matrix image(row_count * column_count, 1);
+		char pixel_buff[1];
+		for (int j = 0; j < row_count * column_count; ++j)
+		{
+			fr.read(pixel_buff, 1);
+			image.at(j,0) = (double)(uint8_t)pixel_buff[0] / 255.0;
+		}
+		result.push_back(std::move(image));
 	}
-	++it;
-	return image;
+
+	fr.close();
+	return result;
 }
--- NN/examples/MNISTreader.h
+++ NN/examples/MNISTreader.h
@@ -7,40 +7,6 @@
 
 namespace MNIST
 {
-std::vector<matrix> read_label_file(const char *filename);
-std::vector<matrix> read_image_file(const char *filename);
-
-class file
-{
-protected:
-	static const uint32_t magic_number;
-	uint32_t count;
-	int it;
-	std::ifstream fstrm;
-	bool is_end = false;
-public:
-	void close();
-	bool end() const;
-	virtual matrix next() = 0;
-};
-
-class label_file : public file
-{
-	static const uint32_t magic_number = 2049;
-public:
-	label_file(const char *filename);
-	~label_file();
-	matrix next() override;
-};
-
-class image_file : public file
-{
-	static const uint32_t magic_number = 2051;
-	uint32_t row_count, column_count;
-public:
-	image_file(const char *filename);
-	~image_file();
-	matrix next() override;
-};
-
+	std::vector<matrix> read_label_file(const char *filename);
+	std::vector<matrix> read_image_file(const char *filename);
 }
